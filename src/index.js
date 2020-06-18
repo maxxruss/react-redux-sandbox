88a81b971e280c639f166fd905973f535c3d7bbf
Игнатьев Максим Юрьевич <ignatev.my@kry.local>
2020-06-18 11:20:00 +0700

    переход на react

--- src/index.js
+++ src/index.js
@@ -1,23 +1,46 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, bindActionCreators } from "redux";
+import Counter from "./counter";
 import reducer from "./reducer";
-import { inc, dec, rnd } from "./actions";
+// import { inc, dec, rnd } from "./actions";
+import * as actions from "./actions";
 
 const store = createStore(reducer);
-document
-  .getElementById("inc")
-  .addEventListener("click", () => store.dispatch(inc()));
+//dispatch написана так, чтобы работать со своим store
+const { dispatch } = store;
 
-document
-  .getElementById("dec")
-  .addEventListener("click", () => store.dispatch(dec()));
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//   dispatch(creator(...args));
+// };
+// const incDispatch = bindActionCreators(inc, dispatch);
+// const decDispatch = bindActionCreators(dec, dispatch);
+// const rndDispatch = bindActionCreators(rnd, dispatch);
 
-document.getElementById("rnd").addEventListener("click", () => {
-  const payload = Math.floor(Math.random() * 10);
-  store.dispatch(rnd(payload));
-});
+// document.getElementById("inc").addEventListener("click", inc);
+// document.getElementById("dec").addEventListener("click", dec);
+// document.getElementById("rnd").addEventListener("click", () => {
+//   const payload = Math.floor(Math.random() * 10);
+//   rnd(payload);
+// });
+
+const { inc, dec, rnd } = bindActionCreators(actions, dispatch);
 
 const update = () => {
-  document.getElementById("counter").innerHTML = store.getState();
+  ReactDOM.render(
+    <Counter
+      counter={store.getState()}
+      inc={inc}
+      dec={dec}
+      rnd={() => {
+        const value = Math.floor(Math.random() * 10);
+        rnd(value);
+      }}
+    />,
+    document.getElementById("root")
+  );
 };
+
+update();
 
 store.subscribe(update);
